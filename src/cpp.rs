68ee21b93cedcbce21661a8f6d9b37e98bce095a
FatherOfEgg <fatherofegg@gmail.com>
2025-03-17 13:45:29 -0400

    Updated BattleObject to have entry_id field

--- src/cpp.rs
+++ src/cpp.rs
@@ -196,7 +196,7 @@ pub mod root {
             pub vtable: *const *const skyline::libc::c_void,
             pub battle_object_id: u32,
             pub kind: u32,
-            _x10: u64,
+            pub entry_id: u64,
             pub agent_kind_hash: root::phx::Hash40,
             pub module_accessor: *mut root::app::BattleObjectModuleAccessor
         }
